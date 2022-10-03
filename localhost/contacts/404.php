1e5309f81cdcd8bd983e37519b69faf1ab231f36
Antonio Bruno <abruno@artiso.com>
2022-10-03 17:33:29 +0200

    404

--- localhost/contacts/404.php
+++ localhost/contacts/404.php
@@ -2,6 +2,73 @@
 require_once "inc/config.inc.php";
 require_once "inc/helper.inc.php";
 
+
+class Rectangle {
+
+	private float $width, $height;
+
+	public function __construct( float $width, float $height ) {
+
+		$this->width  = $width;
+		$this->height = $height;
+
+	}
+
+	public function setWidth( $width ): void {
+		$this->width = $width;
+	}
+
+	public function setHeight( $height ): void {
+		$this->height = $height;
+	}
+}
+
+class Square extends Rectangle {
+
+	public function setWidth( $width ): void {
+		parent::setWidth( $width );
+		$this->width = $width;
+	}
+
+	public function setHeight( $height ): void {
+		parent::setWidth( $height );
+		$this->height = $height;
+	}
+
+}class Rectangle {
+
+	private float $width, $height;
+
+	public function __construct( float $width, float $height ) {
+
+		$this->width  = $width;
+		$this->height = $height;
+
+	}
+
+	public function setWidth( $width ): void {
+		$this->width = $width;
+	}
+
+	public function setHeight( $height ): void {
+		$this->height = $height;
+	}
+}
+
+class Square extends Rectangle {
+
+	public function setWidth( $width ): void {
+		parent::setWidth( $width );
+		$this->width = $width;
+	}
+
+	public function setHeight( $height ): void {
+		parent::setWidth( $height );
+		$this->height = $height;
+	}
+
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
